2a6b021a35582354c9942b56784e45f79248ea78
Junaid Farooq <junaid@evercam.io>
2018-02-21 13:14:16 +0500

    Removed nvr_reboot references from NVR Controller.

--- lib/evercam_media_web/controllers/nvr_controller.ex
+++ lib/evercam_media_web/controllers/nvr_controller.ex
@@ -1,6 +1,5 @@
 defmodule EvercamMediaWeb.NVRController do
   use EvercamMediaWeb, :controller
-  use PhoenixSwagger
   alias EvercamMediaWeb.SnapshotExtractorView
   alias EvercamMediaWeb.ErrorView
   alias EvercamMedia.HikvisionNVR
@@ -104,43 +103,6 @@ defmodule EvercamMediaWeb.NVRController do
       end
     end
   end
-
-  swagger_path :reboot do
-    post "/v1/cameras/nvr/reboot"
-    description "Nvr credentials for rebooting."
-    summary "Reboot nvr."
-    parameters do
-      api_key :query, :string, "", required: true
-      api_id :query, :string, "", required: true
-      url :query, :string, "", required: true
-      port :query, :string, "", required: true
-      user :query, :string, "", required: true
-      password :query, :string, "", required: true
-    end
-    response 201, "Success"
-  end
-
-  def reboot(conn, %{"url" => url, "port" => port, "user" => user, "password" => password}) do
-    with %User{} <- conn.assigns[:current_user] do
-      run_porcelain_shell(url, port, user, password, conn)
-    else
-      nil ->
-        render_error(conn, 401, "Unauthorized")
-    end
-  end
-
-  defp run_porcelain_shell(url, port, user, password, conn) do
-    Porcelain.shell("nvr_reboot #{url} #{port} #{user} #{password}")
-    |> porcelain_output(conn)
-  end
-
-  defp porcelain_output(%Porcelain.Result{err: nil, out: output, status: 0}, conn) do
-    case String.match?(output, ~r/success/) do
-      true -> conn |> put_status(201) |> json(%{reboot: true})
-      _ -> render_error(conn, 400, "Failed to reboot NVR.")
-    end
-  end
-  defp porcelain_output(_, conn), do: render_error(conn, 400, "Failed to reboot NVR.")
 
   defp snapshot_extractor_changeset(config, id, requester, user) do
     params =
