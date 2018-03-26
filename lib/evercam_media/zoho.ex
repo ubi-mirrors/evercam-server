88a19f54120df05cb682073feddecc4bd6e4d4f4
Azhar Malik <azharmalik02@gmail.com>
2018-03-26 11:16:33 +0500

    Commit pending changes

--- lib/evercam_media/zoho.ex
+++ lib/evercam_media/zoho.ex
@@ -33,6 +33,40 @@ defmodule EvercamMedia.Zoho do
     end
   end
 
+  def update_camera(cameras) do
+    url = "#{@zoho_url}xml/CustomModule4/insertRecords?authtoken=#{@zoho_auth_token}&scope=crmapi&newFormat=2"
+    headers = ["Accept": "application/json", "Content-Type": "application/x-www-form-urlencoded"]
+    camera_xml = create_request_xml(cameras, "xmlData=<Cameras>", 1)
+    camera_xml = "#{camera_xml}</Cameras>"
+
+    case HTTPoison.post!(url, camera_xml, headers) do
+      %HTTPoison.Response{body: body} -> {:ok, body}
+      _ -> {:error}
+    end
+  end
+
+  def get_contact(email) do
+    url = "#{@zoho_url}json/Contacts/searchRecords?authtoken=#{@zoho_auth_token}&scope=crmapi&newFormat=2&criteria=(Email:#{email})"
+    headers = ["Accept": "application/offset+octet-stream", "Content-Type": "multipart/form-data"]
+    response = HTTPoison.get(url, headers) |> elem(1)
+
+    case response.status_code do
+      200 ->
+        json_response = Poison.decode!(response.body)
+        case Util.deep_get(json_response, ["response", "nodata", "code"], nil) do
+          "4422" -> {:nodata, Util.deep_get(json_response, ["response", "nodata", "message"], "")}
+          _ ->
+            zoho_contact =
+              case Util.deep_get(json_response, ["response", "result", "Contacts", "row"]) do
+                %{"FL" => contact} -> contact |> List.first
+                contacts -> contacts |> List.first |> Util.deep_get(["FL"])
+              end
+            {:ok, zoho_contact}
+        end
+      _ -> {:error, response}
+    end
+  end
+
   defp create_request_xml([camera | rest], camera_xml, index) do
     url_to_nvr = "http://#{Camera.host(camera, "external")}:#{Camera.get_nvr_port(camera)}"
     camera_xml = "#{camera_xml}<row no=\"#{index}\"><FL val=\"Camera Name\">#{camera.name}</FL><FL val=\"Evercam ID\">#{camera.exid}</FL>
--- lib/mix/tasks/sync_evercam_to_zoho.ex
+++ lib/mix/tasks/sync_evercam_to_zoho.ex
@@ -1,5 +1,7 @@
 defmodule EvercamMedia.SyncEvercamToZoho do
   alias EvercamMedia.Zoho
+  alias EvercamMedia.Repo
+  import Ecto.Query
   require Logger
 
   def sync_cameras(email_or_username) do
@@ -14,4 +16,47 @@ defmodule EvercamMedia.SyncEvercamToZoho do
 
     Logger.info "Camera(s) sync successfully."
   end
+
+  def sync_camera_sharees(email_or_username) do
+    user = User.by_username_or_email(email_or_username)
+    cameras = Camera.for(u, false)
+    camera_ids = Enum.map(cameras, fn(camera) -> camera.id end)
+
+    CameraShare
+    |> where([cs]], cs.user_id in ^camera_ids)
+    |> Repo.all
+
+    # {users, emails} =
+    #   Camera
+    #   |> where([cam], cam.owner_id == ^user.id)
+    #   |> preload(:owner)
+    #   |> preload(:shares)
+    #   |> preload([shares: :user])
+    #   |> Repo.all
+    #   |> Enum.reduce({[], []}, fn(camera, {all_sharees, all_emails}) ->
+    #     {sharees, sharee_emails_list} =
+    #       camera.shares
+    #       |> Enum.reduce({[], all_emails}, fn(camera_share, {sharee_list, sharee_emails}) ->
+    #         # Logger.info "Start Contact sync: [#{camera_share.user.email}]"
+    #         case Enum.member?(sharee_emails, camera_share.user.email) do
+    #           true ->
+    #             Logger.info "Duplicate Email: #{camera_share.user.email}"
+    #             {sharee_list, sharee_emails}
+    #           false ->
+    #             {[camera_share.user | sharee_list], [camera_share.user.email | sharee_emails]}
+    #         end
+    #         # case Zoho.get_contact(camera_share.user.email) do
+    #         #   {:ok, _} -> sharee_list
+    #         #   _ -> [camera_share.user | sharee_list]
+    #         # end
+    #       end)
+    #     {[sharees | all_sharees], [sharee_emails_list | all_emails]}
+    #   end)
+    # IO.inspect Enum.coun(users)
+    # Enum.each(users, fn(user) ->
+    #   Logger.debug user.email
+    # end)
+  end
+
+  defp
 end
