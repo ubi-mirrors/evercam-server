dbb73684a43295b3e3c13dd46fb2a8e1143dea1e
Azhar Malik <azharmalik02@gmail.com>
2018-03-27 12:39:21 +0500

    Removed inspect lines

--- lib/evercam_media_web/controllers/camera_controller.ex
+++ lib/evercam_media_web/controllers/camera_controller.ex
@@ -256,8 +256,7 @@ defmodule EvercamMediaWeb.CameraController do
         {:nodata, _} -> Logger.info "[update_camera_to_zoho] [#{camera.exid}] [Camera does not exists]"
         {:ok, zoho_camera} ->
           record_id = zoho_camera |> List.first |> Map.fetch!("content")
-          response = Zoho.update_camera([camera], record_id)
-          IO.inspect response
+          Zoho.update_camera([camera], record_id)
       end
     end
   end
