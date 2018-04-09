45eff61805d9d05e8289a8a0498d2962903c9939
Junaid Farooq <junaid@evercam.io>
2018-04-09 14:47:12 +0500

    Typo in MoveClipsToS3 both globals pointing towards same seaweed master.

--- lib/mix/tasks/move_clips_to_s3.ex
+++ lib/mix/tasks/move_clips_to_s3.ex
@@ -2,7 +2,7 @@ defmodule EvercamMedia.MoveClipsToS3 do
   require Logger
 
   @bucket "evercam-camera-assets"
-  @seaweedfs Application.get_env(:evercam_media, :seaweedfs_url_1)
+  @seaweedfs Application.get_env(:evercam_media, :seaweedfs_url)
   @seaweedfs_1 Application.get_env(:evercam_media, :seaweedfs_url_1)
 
   def run do
@@ -11,9 +11,9 @@ defmodule EvercamMedia.MoveClipsToS3 do
       with :ok <- not_a_url(archive.url) do
 
         src_object = [
-          "#{which_weed_it_is(archive.created_at)}/#{archive.camera.exid}/clips/#{archive.exid}",
-          "#{which_weed_it_is(archive.created_at)}/#{archive.camera.exid}/clips/#{archive.exid}",
-          "#{which_weed_it_is(archive.created_at)}/#{archive.camera.exid}/clips/thumb-#{archive.exid}"
+          "#{archive.camera.exid}/clips/#{archive.exid}",
+          "#{archive.camera.exid}/clips/#{archive.exid}",
+          "#{archive.camera.exid}/clips/thumb-#{archive.exid}"
         ]
 
         dest_object = [
@@ -58,26 +58,14 @@ defmodule EvercamMedia.MoveClipsToS3 do
   defp put_type("jpg"), do: "image/jpeg"
   defp put_type("mp4"), do: "video/mp4"
 
-  defp which_weed_it_is(date) do
-    actual_date =
-      date
-      |> Ecto.DateTime.to_erl
-      |> Calendar.DateTime.from_erl!("Etc/UTC")
-
-    crash_date =
-      Calendar.DateTime.from_erl!({{2017,11,1},{00,00,00}}, "Etc/UTC")
-
-    case Calendar.DateTime.diff(actual_date, crash_date) do
-      {:ok, _, _, :before} -> @seaweedfs
-      {:ok, _, _, :after} -> @seaweedfs_1
-      {:ok, _, _, :same_time} -> @seaweedfs_1
-    end
-  end
-
   defp exist_on_seaweed?(src, ext) do
-    case HTTPoison.get("#{src}.#{ext}", ["Accept": "application/json"], hackney: [pool: :seaweedfs_download_pool, recv_timeout: 30_000_000]) do
+    case HTTPoison.get("#{@seaweedfs}/#{src}.#{ext}", ["Accept": "application/json"], hackney: [pool: :seaweedfs_download_pool, recv_timeout: 30_000_000]) do
       {:ok, %HTTPoison.Response{status_code: 200, body: data}} -> {:ok, data}
-      _error -> :not_found
+      _error ->
+        case HTTPoison.get("#{@seaweedfs_1}/#{src}.#{ext}", ["Accept": "application/json"], hackney: [pool: :seaweedfs_download_pool, recv_timeout: 30_000_000]) do
+          {:ok, %HTTPoison.Response{status_code: 200, body: data}} -> {:ok, data}
+          _error -> :not_found
+        end
     end
   end
 
