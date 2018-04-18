582649d845f661fa033fa7d66ec9f85bd26ff933
Ali Imran <33378049+Ali7862@users.noreply.github.com>
2018-04-18 17:08:32 +0500

    Added swagger endpoint for compare (create compare). (#920)
    * Added swagger endpoint for compare (create compare).
    
    * Added missing parameters.

--- lib/evercam_media_web/controllers/compare_controller.ex
+++ lib/evercam_media_web/controllers/compare_controller.ex
@@ -85,6 +85,28 @@ defmodule EvercamMediaWeb.CompareController do
     end
   end
 
+  swagger_path :create do
+    post "/cameras/{id}/compares"
+    summary "Create new compare."
+    parameters do
+      id :path, :string, "Unique identifier for the camera.", required: true
+      exid :query, :string, "Unique identifier for the compare.", required: true
+      name :query, :string, "", required: true
+      before_date :query, :string, "Unix timestamp", required: true
+      before_image :query, :string, "Before image in base64 format.", required: true
+      after_date :query, :string, "Unix timestamp", required: true
+      after_image :query, :string, "After image in base64 format.", required: true
+      embed :query, :string, "", required: true
+      create_animation :query, :boolean, "", required: true
+      api_id :query, :string, "The Evercam API id for the requester."
+      api_key :query, :string, "The Evercam API key for the requester."
+    end
+    tag "Compares"
+    response 201, "Success"
+    response 401, "Invalid API keys or Unauthorized"
+    response 404, "Camera does not exist"
+  end
+
   def create(conn, %{"id" => camera_exid} = params) do
     current_user = conn.assigns[:current_user]
     camera = Camera.get_full(camera_exid)
